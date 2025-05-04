5bd982e71ac88a6d31b6bd7424d05f2a6ba5000f
cape-cod <68728926+cape-cod@users.noreply.github.com>
2025-05-04 20:35:55 +0500

    Add files via upload

---
+++ src/config/destinations.js
@@ -0,0 +1,15 @@
+module.exports = {
+  countries: [
+    { code: 'turkey', name: 'Турция' },
+    { code: 'russia', name: 'Россия' },
+    { code: 'uae', name: 'ОАЭ' },
+    { code: 'cyprus', name: 'Кипр' },
+    { code: 'zanzibar', name: 'Занзибар' },
+    { code: 'cuba', name: 'Куба' },
+    { code: 'abkhazia', name: 'Абхазия' }
+  ],
+  russianResorts: ['sochi', 'crimea', 'anapa', 'kalin'],
+  turkishResorts: ['ayt', 'ege', 'ist'],
+  departureCities: ['MOW', 'REN', 'KZN', 'KUF', 'OVB', 'LED', 'SVX', 'UFA', 'KRR'],
+  durations: ['2-3', '4-5', '6-7', '8-9', '10-12', '13-14', '15-19', '20-25']
+};
